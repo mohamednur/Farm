207607b749106a394b0ad3119b65e198b344354e
mohamednur <mohamednurhassan8@gmail.com>
2019-06-12 15:36:17 +0300

    second commit

--- farm/views.py
+++ farm/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth import authenticate, login
 from .forms import ExtendedUserCreationForm, UserprofileForm, CropsForm, addProduce
-from .models import User
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import User, Crops
 
 
 @login_required
@@ -63,15 +66,12 @@ def Add_crops(request):
 
 @login_required
 def Add_produce(request):
-    if request.method == 'POST':
-        produce_form = addProduce(request.POST)
 
-        if produce_form.is_valid():
-            produce = produce_form.save()
+    produce_form = addProduce(request.POST)
 
-            produce.save()
+    if produce_form.is_valid():
+        produce = produce_form.save()
 
-            return redirect('#')
     else:
         produce_form = addProduce()
 
@@ -79,7 +79,54 @@ def Add_produce(request):
     return render(request, 'farm/addProduce.html', context)
 
 
-class UserListView(class ModelNameList(ListView):
-                   model=User
-                   context_object_name='user_list'
-                   template_name='farm/user_list')
+def success(request):
+    return render(request, 'auth/success.html')
+
+
+class UserListView(ListView):
+    model = User
+    context_object_name = 'user_list'
+    template_name = ('auth/user_list.html')
+
+
+class UserDetailView(DeleteView):
+    model = User
+    context_object_name = 'user_detail'
+    template_name = ('auth/user_detail.html')
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['username', 'first_name', 'last_name',
+              'email', 'is_active', 'is_staff']
+
+    success_url = reverse_lazy('user_list')
+
+
+class UserDeleteview(DeleteView):
+    model = User
+    success_url = reverse_lazy('user_list')
+
+
+class CropListView(ListView):
+    model = Crops
+    context_object_name = 'crop_list'
+    template_name = ('farm/crops_list.html')
+
+
+class CropUpdateView(UpdateView):
+    model = Crops
+    fields = '__all__'
+
+    success_url = reverse_lazy('crops_list')
+
+
+class CropDeleteView(DeleteView):
+    model = Crops
+    success_url = reverse_lazy('crops_list')
+
+
+class CropsDetailView(DeleteView):
+    model = Crops
+    context_object_name = 'crop_detail'
+    template_name = ('farm/crops_detail.html')
